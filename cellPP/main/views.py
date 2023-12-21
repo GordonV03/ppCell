503a8cc12baed00f324bd6571a90ca8d31e605b8
Maksiz03 <92305927+Maksiz03@users.noreply.github.com>
2023-12-21 19:16:32 +0500

    2

--- cellPP/main/views.py
+++ cellPP/main/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.db import models
+from django.forms import fields
+from .models import UploadImage
+from django import forms
 # Create your views here.
+from django.shortcuts import redirect, render
+
+from .models import UploadImage
+
 
 def index(request):
     return render(request, 'main/index.html')
