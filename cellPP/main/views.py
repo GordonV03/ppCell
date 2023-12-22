6fc5069f3dfc4c903a15ad5304eae8595b9261ed
Maksiz03 <92305927+Maksiz03@users.noreply.github.com>
2023-12-22 19:42:17 +0500

    3

--- cellPP/main/views.py
+++ cellPP/main/views.py
@@ -1,17 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UploadedFile, UploadedImage
+from .forms import UploadFileForm, UploadFileForm2, UploadImageForm
 from django.http import HttpResponse
-from django.db import models
-from django.forms import fields
-from .models import UploadImage
-from django import forms
-# Create your views here.
-from django.shortcuts import redirect, render
 
-from .models import UploadImage
+def upload_and_display_files(request):
+    files = UploadedFile.objects.all()
 
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for uploaded_file in request.FILES.getlist('files'):
+                UploadedFile.objects.create(file=uploaded_file)
+            return redirect('upload_and_display')
+    else:
+        form = UploadFileForm()
 
-def index(request):
-    return render(request, 'main/index.html')
+    return render(request, 'upload_and_display.html', {'form': form, 'files': files})
 
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm2(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('upload_file')
+    else:
+        form = UploadFileForm2()
+    files = UploadedFile.objects.all()
+    return render(request, 'upload_file.html', {'form': form, 'files': files})
 
+def download_file(request, file_id):
+    uploaded_file = UploadedFile.objects.get(pk=file_id)
+    response = HttpResponse(uploaded_file.file, content_type='application/force-download')
+    response['Content-Disposition'] = f'attachment; filename="{uploaded_file.file.name}"'
+    return response
 
+def upload_image(request):
+    if request.method == 'POST':
+        form = UploadImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UploadImageForm()
+    images = UploadedImage.objects.all()
+    return render(request, 'upload_image.html', {'form': form, 'images': images})
