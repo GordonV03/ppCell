3e1d96ba2aa0644f6db15accd45c618ffaf5d66d
GordonV03 <92227408+GordonV03@users.noreply.github.com>
2023-12-23 23:17:20 +0500

    api

--- cellPP/main/views.py
+++ cellPP/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from .models import UploadedFile, UploadedImage
 from .forms import UploadFileForm, UploadFileForm2, UploadImageForm
 from django.http import HttpResponse
+from roboflow import Roboflow
+
 
 def upload_and_display_files(request):
     files = UploadedFile.objects.all()
@@ -17,6 +19,7 @@ def upload_and_display_files(request):
 
     return render(request, 'upload_and_display.html', {'form': form, 'files': files})
 
+
 def upload_file(request):
     if request.method == 'POST':
         form = UploadFileForm2(request.POST, request.FILES)
@@ -25,14 +28,22 @@ def upload_file(request):
             return redirect('upload_file')
     else:
         form = UploadFileForm2()
-    files = UploadedFile.objects.all()
+    files = UploadedFile.objects.all()[len(UploadedFile.objects.all())-1:]
     return render(request, 'upload_file.html', {'form': form, 'files': files})
+
 
 def download_file(request, file_id):
     uploaded_file = UploadedFile.objects.get(pk=file_id)
+    rf = Roboflow(api_key="9HvM7C6aQdtPzxBtG6R4")
+    project = rf.workspace().project("cell-detection-bnw1g")
+    model = project.version(11).model
+    img_file = 'media/' + uploaded_file.file.name
+    prediction = model.predict(img_file, confidence=40, overlap=30)
+    prediction.save(output_path=f'media/{uploaded_file.file.name}')
     response = HttpResponse(uploaded_file.file, content_type='application/force-download')
     response['Content-Disposition'] = f'attachment; filename="{uploaded_file.file.name}"'
     return response
+
 
 def upload_image(request):
     if request.method == 'POST':
